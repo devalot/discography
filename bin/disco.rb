12f397dd91500cb10e0b05f90af124cdf9101466
ddd1600 <ddd1600@gmail.com>
2012-06-18 21:34:37 -0500

    more ruby-like?

--- bin/disco.rb
+++ bin/disco.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 ################################################################################
-$:.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
+$LOAD_PATH.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
 
 ################################################################################
 require("discography")
